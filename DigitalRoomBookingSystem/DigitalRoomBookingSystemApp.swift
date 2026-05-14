b995bf948ede2e907a4ba80327402b4cc42a26e4
evelinn11 <evelinalim20@gmail.com>
2026-05-14 20:39:08 +0700

    Initial commit: Add base Xcode project

---
+++ DigitalRoomBookingSystem/DigitalRoomBookingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalRoomBookingSystemApp.swift
+//  DigitalRoomBookingSystem
+//
+//  Created by Evelin Alim Natadjaja on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DigitalRoomBookingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
